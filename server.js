2396cfc4e3c3ea192c844c187d3361fc5117dc33
Javin-Ambridge <javin.ambridge@gmail.com>
2017-01-11 16:37:35 -0500

    removing this

--- server.js
+++ server.js
@@ -17,24 +17,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));  
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://aqueous-thicket-51307.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 //
 firebase.initializeApp({
   serviceAccount: "./config/lolvideo-7c00d5772a07.json",
