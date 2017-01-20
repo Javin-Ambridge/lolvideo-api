ebcd404448a38af10ad124ff15783766352d266d
Javin-Ambridge <javin.ambridge@gmail.com>
2017-01-20 17:39:14 -0500

    Removed refreshing videos

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -2,15 +2,6 @@ var CronJob = require('cron').CronJob;
 var Video = require('../api/video');
 
 module.exports.cronJob = function() {
-	var job = new CronJob({
-	  cronTime: '1 59 * * * *',
-	  onTick: function() {
-	    console.log('GRAB DATA');
-	    Video.refreshVideos();
-	  },
-	  runOnInit: true,
-	  start: false
-	});
 	var refreshCache = new CronJob({
 	  cronTime: '1 */15 * * * *',
 	  onTick: function() {
@@ -20,6 +11,5 @@ module.exports.cronJob = function() {
 	  runOnInit: true,
 	  start: false
 	});
-	job.start();
 	refreshCache.start();
 };
