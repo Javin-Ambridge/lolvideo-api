3f63fd134ae476f684d8a473569bb99adc73f130
Javin-Ambridge <javin.ambridge@gmail.com>
2017-01-21 14:25:22 -0500

    fixed pinging

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 var Video = require('../api/video');
-var ping = require('ping');
+var https = require('https');
 
 module.exports.cronJob = function() {
 	var refreshCache = new CronJob({
@@ -16,11 +16,13 @@ module.exports.cronJob = function() {
 		cronTime: '1 */15 7-23 * * *',
 		onTick: function() {
 			console.log('Pinging Updater');
-			var host = 'https://lolvideo-ghetto-worker.herokuapp.com/';
-			ping.sys.probe(host, function(isAlive) {
-		        var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-		        console.log(msg);
-			});
+			var options = {
+			  host: 'lolvideo-ghetto-worker.herokuapp.com',
+			  port: 443,
+			  method: 'GET'
+			};
+			var getReq = https.request(options, function(res) {});
+			getReq.end();
 		},
 		runOnInit: true,
 		start: false
